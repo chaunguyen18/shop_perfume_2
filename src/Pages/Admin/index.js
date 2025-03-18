1dd255b68ec84af5d6d5bd3e2d931b8765a7bd7c
Chau Nguyen <chaunguyen18@gmail.com>
2025-03-19 01:15:03 +0700

    update admin page

--- src/Pages/Admin/index.js
+++ src/Pages/Admin/index.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import HeaderAdmin from '../../Components/HeaderAdmin'
+import FooterAdmin from '../../Components/FooterAdmin'
+import SidebarAdmin from '../../Components/SidebarAdmin'
 
 const Admin = () => {
   return (
-    <div>Chào mừng đến với trang Admin</div>
+    <div>
+      <HeaderAdmin />
+      <div className='container-fluid'>
+        <div className='row'>
+          <div className='col-md-5'>
+            <SidebarAdmin />
+          </div>
+          <div className='col-md-7 admin-content'>
+            Admin content
+          </div>
+        </div>
+      </div>
+      <FooterAdmin />
+    </div>
   )
 }
 
